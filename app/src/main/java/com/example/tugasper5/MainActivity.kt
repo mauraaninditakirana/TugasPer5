095a896452ecc314d0cfec8be0a0ecbf05198137
mauraaninditakirana <kiranamaura351@gmail.com>
2025-10-31 17:57:18 +0700

    Memanggil FormulirPendaftaran di MainActivity.kt

--- app/src/main/java/com/example/tugasper5/MainActivity.kt
+++ app/src/main/java/com/example/tugasper5/MainActivity.kt
@@ -20,8 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             TugasPer5Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    FormulirPendaftaran(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
